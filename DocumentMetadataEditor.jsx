ffc5331fbc383732aeca2344bd158e5516dccb2a
geeklystrips <pattesdours@gmail.com>
2017-09-28 00:51:22 -0400

    Added DocumentMetadataEditor.jsx (0.43)
    - updated LayerMetadataEditor.jsx to support document-level XMP
    - added DocumentMetadataEditor.jsx (useDoc boolean, includes LayerMetadataEditor.jsx)

---
+++ DocumentMetadataEditor.jsx
@@ -0,0 +1,12 @@
+﻿/*
+	DocumentMetadataEditor.jsx (XMP operations)
+	Source: https://github.com/geeklystrips/pslib
+*/
+
+#target photoshop
+
+// this variable decides whether you're reading/storing information from/to Layer metadata or Document Metadata
+var useDoc = true;
+
+//@show include
+//@include "LayerMetadataEditor.jsx"
